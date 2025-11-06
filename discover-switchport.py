9ce744d5be2a3c7a7360c26e4db628b3cad70b3b
Vherolf <vherolf@procloc.com>
2025-11-06 12:07:33 +0100

    added args

--- discover-switchport.py
+++ discover-switchport.py
@@ -2,6 +2,7 @@
 
 # parse for a cdp package to find on which remote Cisco switch and port its connected 
 from scapy.all import *
+import argparse
 
 load_contrib("cdp")
  
@@ -20,7 +21,23 @@ def cdp_monitor_callback(pkt):
       ip=pkt["CDPAddrRecordIPv4"].addr
     return f"Device: {switchname} Port: {port} VLAN: {vlan} IP: {ip}"
  
-interface="eno1"
+interface="enp1s0"
 capturefilter="ether dst 01:00:0c:cc:cc:cc"
  
-p=sniff(prn=cdp_monitor_callback, iface=interface, count=1, filter=capturefilter, store=0)
+def main(interface=interface, capturefilter=capturefilter):
+    print("Listening for CDP packets on interface", interface)
+    p=sniff(prn=cdp_monitor_callback, iface=interface, count=1, filter=capturefilter, store=0)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="switchport discovery via CDP",
+                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+  parser.add_argument('-cf','--capturefilter', type=str, default='ether dst 01:00:0c:cc:cc:cc', help='capture filter for sniffing')
+  parser.add_argument('-i','--interface', type=str, default='enp1s0', help='network interface to listen on')
+  args = parser.parse_args()
+  if args.interface:
+      interface=args.interface
+  if args.capturefilter:
+      capturefilter=args.capturefilter
+
+  main(interface=interface, capturefilter=capturefilter)  
