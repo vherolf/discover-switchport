347d9c001605ce2824a68eb1e8e68307eef66a3d
Vherolf <vherolf@procloc.com>
2025-11-06 13:21:26 +0100

    fixe intendation

--- discover-switchport.py
+++ discover-switchport.py
@@ -7,20 +7,20 @@ import argparse
 load_contrib("cdp")
  
 def cdp_monitor_callback(pkt):
-  ip = "0.0.0.0"
-  if (CDPMsgDeviceID in pkt):
-    device=pkt["CDPMsgDeviceID"].val.decode()
-    hostname=device.split(".")[0]
-    if (CDPMsgPortID in pkt):
-      port = pkt["CDPMsgPortID"].iface.decode()
+    ip = "0.0.0.0"
     if (CDPMsgDeviceID in pkt):
-      switchname = pkt["CDPMsgDeviceID"].val.decode()
-    if (CDPMsgNativeVLAN in pkt):
-      vlan = pkt["CDPMsgNativeVLAN"].vlan
-    if (CDPAddrRecordIPv4 in pkt):
-      ip=pkt["CDPAddrRecordIPv4"].addr
-    return f"Device: {switchname} Port: {port} VLAN: {vlan} IP: {ip}"
- 
+      device=pkt["CDPMsgDeviceID"].val.decode()
+      hostname=device.split(".")[0]
+      if (CDPMsgPortID in pkt):
+        port = pkt["CDPMsgPortID"].iface.decode()
+      if (CDPMsgDeviceID in pkt):
+        switchname = pkt["CDPMsgDeviceID"].val.decode()
+      if (CDPMsgNativeVLAN in pkt):
+        vlan = pkt["CDPMsgNativeVLAN"].vlan
+      if (CDPAddrRecordIPv4 in pkt):
+        ip=pkt["CDPAddrRecordIPv4"].addr
+      return f"Device: {switchname} Port: {port} VLAN: {vlan} IP: {ip}"
+  
 interface="enp1s0"
 capturefilter="ether dst 01:00:0c:cc:cc:cc"
  
@@ -30,14 +30,14 @@ def main(interface=interface, capturefilter=capturefilter):
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="switchport discovery via CDP",
-                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('-cf','--capturefilter', type=str, default='ether dst 01:00:0c:cc:cc:cc', help='capture filter for sniffing')
-  parser.add_argument('-i','--interface', type=str, default='enp1s0', help='network interface to listen on')
-  args = parser.parse_args()
-  if args.interface:
-      interface=args.interface
-  if args.capturefilter:
-      capturefilter=args.capturefilter
+    parser = argparse.ArgumentParser(description="switchport discovery via CDP",
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-cf','--capturefilter', type=str, default='ether dst 01:00:0c:cc:cc:cc', help='capture filter for sniffing')
+    parser.add_argument('-i','--interface', type=str, default='enp1s0', help='network interface to listen on')
+    args = parser.parse_args()
+    if args.interface:
+        interface=args.interface
+    if args.capturefilter:
+        capturefilter=args.capturefilter
 
-  main(interface=interface, capturefilter=capturefilter)  
+    main(interface=interface, capturefilter=capturefilter)  
